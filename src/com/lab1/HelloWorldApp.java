ff50ce1dcf1336ce3c638548a5043de7fe84b54a
Varun Bankar <mail.varunbankar@gmail.com>
2020-08-20 13:12:05 +0530

    Preliminary practice before Lab 1

---
+++ src/com/lab1/HelloWorldApp.java
@@ -0,0 +1,19 @@
+package com.lab1;
+
+/*
+ * The HelloWorldClass implements an application
+ * that displays "Hello World!" to the standard output
+ */
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        // Display "Hello World!"
+        System.out.println("Hello World!");
+
+        // Variable declaration
+        int a = 3;
+        int b = 3;
+
+        // Display multiplication of variable a and b
+        System.out.println("a * b = " + (a * b));
+    }
+}
